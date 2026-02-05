fbed35f16c47165a0e82dd4e7504f3bf9f025028
mishatorgovtsev <mifa20071009@gmail.com>
2026-02-05 18:17:13 +0300

    Add ChessService with ChessDotNet integration, registered in DI

--- kursProjChesh_Torgovcev/Program.cs
+++ kursProjChesh_Torgovcev/Program.cs
@@ -1,4 +1,5 @@
-using ChessPlatform.Data;
+using ChessPlatform.Data;                  // Для ChessDbContext
+using kursProjChesh_Torgovcev.services;    // Для ChessService
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,20 +7,17 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем контекст базы данных SQL Server
 builder.Services.AddDbContext<ChessDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Добавляем контроллеры API
-builder.Services.AddControllers();
+builder.Services.AddScoped<ChessService>();  // <-- ChessService добавлен
 
-// Swagger для тестирования API
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Включаем Swagger только в режиме разработки
 app.UseSwagger();
 app.UseSwaggerUI();
 
---
+++ kursProjChesh_Torgovcev/services/ChessService.cs
@@ -0,0 +1,35 @@
+﻿using ChessDotNet;
+
+namespace kursProjChesh_Torgovcev.services;
+
+public class ChessService
+{
+    private readonly ChessGame _game;
+    
+    public ChessService()
+    {
+        _game = new ChessGame();
+    }
+    
+    public (bool success, string newFen, string pgnMove) MakeMove(string from, string to)
+    {
+        var move = new Move(from, to, _game.WhoseTurn);
+        
+        if (_game.IsValidMove(move))
+        {
+            _game.MakeMove(move, true);
+            return (true, _game.GetFen(), move.ToString());
+        }
+        
+        return (false, _game.GetFen(), string.Empty);
+    }
+    
+    public bool IsGameOver() 
+    { 
+        return _game.IsCheckmated(_game.WhoseTurn) || _game.IsStalemated(_game.WhoseTurn); 
+    }
+    
+    public string GetCurrentFen() => _game.GetFen();
+    
+    public Player WhoseTurn => _game.WhoseTurn;
+}
